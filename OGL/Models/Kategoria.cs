115ab97cc110d0dace07f41e6b8221256bfa6db4
Mariusz <mariusz.maciejczuk@softwarehut.pl>
2016-12-02 13:19:35 +0100

    model changes

---
+++ OGL/Models/Kategoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OGL.Models
+{
+    public class Kategoria
+    {
+        public Kategoria()
+        {
+            this.Ogloszenie_Kategoria = new HashSet<Ogloszenie_Kategoria>();
+        }
+
+
+    }
+}
